021cea98e4d9888220842b23b8087f8601931d96
rakshakshah <rakshak.shah@gmail.com>
2025-03-16 16:52:38 -0400

    using local instead of render

--- server.py
+++ server.py
@@ -40,4 +40,4 @@ def run_python():
     return jsonify({"message": movie_instance + user_text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
